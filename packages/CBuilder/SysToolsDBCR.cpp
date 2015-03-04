8dc9d01913acb7aa8267a0b26d85be1ba64f4640
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-03-04 21:39:54 +0100

    The 64 bit linker for the C++Builder caused problems.

--- packages/CBuilder/SysToolsDBCR.cpp
+++ packages/CBuilder/SysToolsDBCR.cpp
@@ -3,6 +3,9 @@
 #include <System.hpp>
 #pragma hdrstop
 #pragma package(smart_init)
+#ifdef _WIN64
+#pragma comment(lib, "SysToolsCR")
+#endif
 //---------------------------------------------------------------------------
 
 //   Package-Quelltext.
